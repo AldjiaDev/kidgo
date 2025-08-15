d0e8fbc063156b9c19246dbf0ce513df00ea9e3b
Aldjia Boughias <boughias-aldjia@live.fr>
2025-08-15 14:39:00 +0200

    create auth

---
+++ app/(settings)/profile.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react';
+import { Session } from '@supabase/supabase-js';
+
+import Account from '~/components/Account';
+import Auth from '~/components/Auth';
+import { BodyScrollView } from '~/components/ui/BodyScrollView';
+import { supabase } from '~/lib/supabase';
+
+export default function ProfileScreen() {
+  const [session, setSession] = useState<Session | null>(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
+
+  return (
+    <BodyScrollView
+      contentContainerStyle={{
+        paddingVertical: 16,
+        gap: 2,
+      }}>
+      {session && session.user ? <Account key={session.user.id} session={session} /> : <Auth />}
+    </BodyScrollView>
+  );
+}
