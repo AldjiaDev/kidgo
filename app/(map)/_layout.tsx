51cbbf21b9d4e0273f6342367bacbeb0f487a32d
David Leuliette <dleuliette@gmail.com>
2025-09-10 19:22:40 +0200

    fix Maps screen

--- app/(map)/_layout.tsx
+++ app/(map)/_layout.tsx
@@ -1,4 +1,4 @@
-import Stack from '~/components/ui/Stack';
+import { Stack } from 'expo-router';
 
 export const unstable_settings = {
   anchor: 'index',
@@ -11,6 +11,7 @@ export default function TabLayout() {
     <Stack
       screenOptions={{
         title: '',
+        headerShown: false,
       }}
     />
   );
--- app/(map)/index.tsx
+++ app/(map)/index.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { View } from 'react-native';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context';
 
 import { Container } from '~/components/Container';
 import { LocationPermissionBanner } from '~/components/LocationPermissionBanner';
@@ -10,14 +10,13 @@ import { PlacesList } from '~/components/PlacesList';
 
 export default function MapScreen() {
   const statusBarInset = useSafeAreaInsets().top; // inset of the status bar
-  const largeHeaderInset = statusBarInset + 38; // inset to use for a large header since it's frame is equal to 96 + the frame of status bar
 
   const [selectedIndex, setSelectedIndex] = useState(0);
 
   return (
-    <View className="flex-1">
+    <SafeAreaView className="flex-1">
       <LocationPermissionBanner />
-      <View className="absolute left-4 right-4 z-10" style={{ top: largeHeaderInset }}>
+      <View className="absolute left-4 right-4 z-10" style={{ top: statusBarInset }}>
         <SegmentedControl
           values={['Carte', 'Liste']}
           selectedIndex={selectedIndex}
@@ -32,6 +31,6 @@ export default function MapScreen() {
           <PlacesList />
         </Container>
       )}
-    </View>
+    </SafeAreaView>
   );
 }
