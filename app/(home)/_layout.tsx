8b6bfeebb3335e94e94fee691c201f02a3aaaab1
David Leuliette <dleuliette@gmail.com>
2025-09-30 21:11:15 +0200

    upate

--- app/(home)/_layout.tsx
+++ app/(home)/_layout.tsx
@@ -9,25 +9,28 @@ export { ErrorBoundary } from 'expo-router';
 export default function TabLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={INDEX_OPTIONS} />
-      <Stack.Screen name="modal" options={MODAL_OPTIONS} />
-      <Stack.Screen name="feedback" options={FEEDBACK_OPTIONS} />
+      <Stack.Screen
+        name="index"
+        options={{
+          headerLargeTitle: true,
+          title: 'KidGO',
+        }}
+      />
+      <Stack.Screen
+        name="modal"
+        options={{
+          presentation: 'modal',
+          animation: 'fade_from_bottom', // for android
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="feedback"
+        options={{
+          headerLargeTitle: true,
+          title: 'Feedback',
+        }}
+      />
     </Stack>
   );
 }
-
-const INDEX_OPTIONS = {
-  headerLargeTitle: true,
-  title: 'KidGO',
-} as const;
-
-const FEEDBACK_OPTIONS = {
-  headerLargeTitle: true,
-  title: 'Feedback',
-} as const;
-
-const MODAL_OPTIONS = {
-  presentation: 'modal',
-  animation: 'fade_from_bottom', // for android
-  headerShown: false,
-} as const;
