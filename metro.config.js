77fb78277e2defc9022586defe3b054da72908fe
David Leuliette <dleuliette@gmail.com>
2025-12-03 09:52:03 +0100

    do not ship storybook on production

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,4 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
-const path = require('path');
 const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
 const withStorybook = require('@storybook/react-native/metro/withStorybook');
@@ -10,19 +9,8 @@ const config = getDefaultConfig(__dirname);
 
 // First apply Storybook configuration, then NativeWind
 const configWithStorybook = withStorybook(config, {
-  // Set to false to remove storybook specific options
-  // you can also use a env variable to set this
-  enabled: true,
-  // Path to your storybook config
-  configPath: path.resolve(__dirname, './.rnstorybook'),
-  // note that this is the default so you can the config path blank if you use .rnstorybook
-
-  // Optional websockets configuration
-  // Starts a websocket server on the specified port and host on metro start
-  // websockets: {
-  //   port: 7007,
-  //   host: 'localhost',
-  // },
+  // When false, removes Storybook from bundle (useful for production)
+  enabled: process.env.EXPO_PUBLIC_STORYBOOK_ENABLED === 'true',
 });
 
 module.exports = withNativeWind(configWithStorybook, { input: './global.css', inlineRem: 16 });
